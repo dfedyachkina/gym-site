993403ccbcb911d9062bd43111b59ecd5519f116
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-08 22:57:44 +0000

    Add urls in the_welness_gym project

--- the_welness_hub/urls.py
+++ the_welness_hub/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("gym_home.urls"), name="home-urls"),
+    path('summernote/', include('django_summernote.urls')),
 ]
