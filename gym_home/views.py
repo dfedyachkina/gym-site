2be0c8933465e1b25090684d3f1f21bcb62dcaef
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-08 22:55:41 +0000

    Add views in gym_home

--- gym_home/views.py
+++ gym_home/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
+from .models import Carousel, HomeText
+
+def home(request):
+    carousel_images = Carousel.objects.filter(is_active=True)
+    home_text = HomeText.objects.filter(is_active=True).first()
+
+    return render(request, 'gym_home/home.html', {'carousel_images': carousel_images, 'home_text': home_text})
