20518c606de66b2bd00f8ca150a9abaf30daac90
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-13 12:37:58 +0000

    Add custom sign up form for allauth

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from allauth.account.forms import SignupForm
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    date_of_birth = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
+    sex = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')], required=True)
+
+    def save(self, request):
+        user = super().save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.date_of_birth = self.cleaned_data['date_of_birth']
+        user.sex = self.cleaned_data['sex']
+        user.save()
+        return user
