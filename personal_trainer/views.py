3fd7ef0474e3e4e3c00772f12284bb32e89bb2ed
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-13 00:52:39 +0000

    Add views in personal trainer

--- personal_trainer/views.py
+++ personal_trainer/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render, redirect
+from .forms import AppointmentForm
 from .models import Appointment
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def create_appointment(request):
     if request.method == 'POST':
-        user = request.user  
-        date = request.POST.get('date')
-        time = request.POST.get('time')
-        trainer_gender = request.POST.get('trainer_gender')
-        is_member = request.POST.get('is_member') == 'on'
-        notes = request.POST.get('notes')
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.user = request.user
+            appointment.save() 
+            return redirect('appointment_list')
+    else:
+        form = AppointmentForm()
 
-        appointment = Appointment(
-            user=user,
-            date=date,
-            time=time,
-            trainer_gender=trainer_gender,
-            is_member=is_member,
-            notes=notes,
-        )
-        appointment.save()
+    return render(request, 'personal_trainer/create_appointment.html', {'form': form})
 
-        return redirect('appointment_list') 
-
-    return render(request, 'personal_trainer/create_appointment.html')
+def appointment_list(request):
+    appointments = Appointment.objects.filter(user=request.user)
+    return render(request, 'personal_trainer/appointment_list.html', {'appointments': appointments})
