fc1f376a0086290659a8f9a9b18f9becfa1c5bb5
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-13 00:31:21 +0000

    Add urls in urls.py

--- the_welness_hub/urls.py
+++ the_welness_hub/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("gym_home.urls"), name="home-urls"),
+    path("", include("gym_home.urls")),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
+    path('create-appointment/', include('personal_trainer.urls')),
 ]
