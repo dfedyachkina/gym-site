d148b503e64b8a3865198ee7fdee5dde8139a4a2
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-18 21:33:05 +0000

    Create model Membership

--- membership/models.py
+++ membership/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+class Membership(models.Model):
+    MEMBERSHIP_CHOICES = [
+        ('basic', 'Basic Membership'),
+        ('premium', 'Premium Membership'),
+        ('elite', 'Elite Membership'),
+    ]
+    
+    name = models.CharField(max_length=50, choices=MEMBERSHIP_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='membership_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
