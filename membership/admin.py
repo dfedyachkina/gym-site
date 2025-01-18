c4053a7b548daba17f5d1842eb4ba319d1c92583
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-18 21:36:05 +0000

    Add class MrmbershipAdmin in admin

--- membership/admin.py
+++ membership/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Membership, Benefit, MembershipRequest
+
+
+class MembershipAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'price', 'description', 'image')  
+    search_fields = ('name', 'description')
+
+
