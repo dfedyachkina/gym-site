46299664efd82309d5875bff947b174bdae3506e
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-18 21:39:23 +0000

    Add views memebership in views

--- membership/views.py
+++ membership/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Membership, MembershipRequest
+
+def membership(request):
+    memberships = Membership.objects.all()
+    return render(request, 'membership/membership.html', {'memberships': memberships})
+
+
