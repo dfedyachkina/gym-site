68efe5875ebf152c4c1f4365a990808302596149
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-19 20:19:10 +0000

    Add GymContact and Contact in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact, GymContacts
 
-# Register your models here.
+class GymContactAdmin(admin.ModelAdmin):
+    list_display = ['email', 'phone_number']
+    def has_add_permission(self, request):
+        return not GymContacts.objects.exists()
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+admin.site.register(Contact)
+admin.site.register(GymContacts, GymContactAdmin)
