ce51e09acb8078741992df697cb3a8522f0e5afc
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-19 20:16:51 +0000

    Create a model Contact in contacts.madels

--- contact/models.py
+++ contact/models.py
@@ -9,3 +9,13 @@ class GymContacts(models.Model):
         return f"Gym Contact Information"
 
 
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Contact request from {self.first_name} {self.last_name}"
+
