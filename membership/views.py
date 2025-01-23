940fe9f92f164fcb5c6e169f29604668dfa563aa
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-23 19:22:59 +0000

    Changing view request_membership

--- membership/views.py
+++ membership/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Membership, MembershipRequest
 
@@ -7,12 +8,18 @@ def membership(request):
     memberships = Membership.objects.all()
     return render(request, 'membership/membership.html', {'memberships': memberships})
 
-
+@login_required
 def request_membership(request, membership_id):
     try:
         membership = Membership.objects.get(id=membership_id)
     except Membership.DoesNotExist:
         return redirect('membership')
+    
+    if MembershipRequest.objects.filter(user=request.user).exists():
+        membership_request = MembershipRequest.objects.filter(user=request.user).first()
+        membership_request.membership = membership
+        membership_request.save()
+        return redirect('membership_success')
 
     membership_request = MembershipRequest.objects.create(
         user=request.user,
