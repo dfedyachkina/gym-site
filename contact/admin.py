2aa5b7a556f4d4b420c90c59fff2bc445845989d
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-25 15:08:14 +0000

    Fix python validation errors in admin accounts

--- contact/admin.py
+++ contact/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Contact, GymContacts
 
+
 class GymContactAdmin(admin.ModelAdmin):
     list_display = ['email', 'phone_number']
+
     def has_add_permission(self, request):
         return not GymContacts.objects.exists()
 
     def has_change_permission(self, request, obj=None):
         return True
+
 
 admin.site.register(Contact)
 admin.site.register(GymContacts, GymContactAdmin)
