19065cacf7a02fe1fce2edbcd1b838d910473fc6
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-25 15:24:21 +0000

    Fix python validation errors in admin gym_home

--- gym_home/admin.py
+++ gym_home/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Carousel, HomeText
 from django_summernote.admin import SummernoteModelAdmin
 
+
 # Carousel Model Admin
 class CarouselAdmin(admin.ModelAdmin):
     list_display = ('order', 'is_active', 'image_preview')
@@ -11,14 +12,16 @@ class CarouselAdmin(admin.ModelAdmin):
 
     def image_preview(self, obj):
         return obj.image.url if obj.image else "No Image"
-    
+
     image_preview.short_description = 'Image Preview'
+
 
 # HomeText Model Admin
 class HomeTextAdmin(SummernoteModelAdmin):
     list_display = ('title', 'is_active')
     list_filter = ('is_active',)
     search_fields = ('title', 'content')
+
 
 admin.site.register(Carousel, CarouselAdmin)
 admin.site.register(HomeText, HomeTextAdmin)
